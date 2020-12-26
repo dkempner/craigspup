d2a908116f1f5ae087c77a0103811c2772123fbb
Daniel Kempner <dkempner@lyft.com>
2020-12-26 14:53:18 -0800

    title

--- src/pages/puppies.tsx
+++ src/pages/puppies.tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Head from "next/head";
 import { fuzzySearch } from "../services/craigslist-fuzzy";
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -23,26 +24,32 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   return {
     props: {
       searchResults: JSON.parse(JSON.stringify(searchResults)),
+      breeds,
     },
   };
 };
 
-const Puppies = ({ searchResults }) => (
-  <div className="grid grid-cols-4 gap-10">
-    {searchResults.map((r) => (
-      <div className="w-100 border border-dashed border-gray" key={r.pid}>
-        <a href={r.url} className="">
-          <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
-          <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
-          <img
-            className="place-self-center"
-            src={(r.images || [""])[0]}
-            alt=""
-          />
-        </a>
-      </div>
-    ))}
-  </div>
+const Puppies = ({ searchResults, breeds }) => (
+  <>
+    <Head>
+      <title>{breeds.join(", ")}</title>
+    </Head>
+    <div className="grid grid-cols-4 gap-10">
+      {searchResults.map((r) => (
+        <div className="w-100 border border-dashed border-gray" key={r.pid}>
+          <a href={r.url} className="">
+            <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
+            <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
+            <img
+              className="place-self-center"
+              src={(r.images || [""])[0]}
+              alt=""
+            />
+          </a>
+        </div>
+      ))}
+    </div>
+  </>
 );
 
 export default Puppies;
