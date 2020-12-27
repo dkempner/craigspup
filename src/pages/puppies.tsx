a57f2fac901e9a22c227bbb02440a001a7c6a7fa
Daniel Kempner <dkempner@lyft.com>
2020-12-26 16:02:40 -0800

    styling

--- src/pages/puppies.tsx
+++ src/pages/puppies.tsx
@@ -34,20 +34,22 @@ const Puppies = ({ searchResults, breeds }) => (
     <Head>
       <title>{breeds.join(", ")}</title>
     </Head>
-    <div className="grid grid-cols-4 gap-10">
-      {searchResults.map((r) => (
-        <div className="w-100 border border-dashed border-gray" key={r.pid}>
-          <a href={r.url} className="">
-            <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
-            <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
-            <img
-              className="place-self-center"
-              src={(r.images || [""])[0]}
-              alt=""
-            />
-          </a>
-        </div>
-      ))}
+    <div className="container mx-auto">
+      <div className="grid grid-cols-4 gap-10">
+        {searchResults.map((r) => (
+          <div className="w-100 border border-dashed border-gray" key={r.pid}>
+            <a href={r.url} className="">
+              <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
+              <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
+              <img
+                className="place-self-center max-h-72"
+                src={(r.images || [""])[0]}
+                alt=""
+              />
+            </a>
+          </div>
+        ))}
+      </div>
     </div>
   </>
 );
