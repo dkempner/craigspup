2332825a781e9522b3b8024cd162e3a121918948
Daniel Kempner <dkempner@lyft.com>
2020-12-26 17:30:55 -0800

    new tab

--- src/pages/puppies.tsx
+++ src/pages/puppies.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-target-blank */
 import { GetServerSideProps } from "next";
 import Head from "next/head";
 import { fuzzySearch } from "../services/craigslist-fuzzy";
@@ -50,7 +51,7 @@ const Puppies = ({ searchResults, breeds, cities }) => {
         <div className="grid grid-cols-4 gap-10">
           {searchResults.map((r) => (
             <div className="w-100 border border-dashed border-gray" key={r.pid}>
-              <a href={r.url} className="">
+              <a target="_blank" href={r.url} className="">
                 <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
                 <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
                 <img
