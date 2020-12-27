75c349092b7980cf468b097e93ce5861a7ba1269
Daniel Kempner <dkempner@lyft.com>
2020-12-26 17:15:07 -0800

    customize cities

--- src/pages/puppies.tsx
+++ src/pages/puppies.tsx
@@ -6,7 +6,13 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
   const breeds = (context.query.breeds as string | undefined)?.split(",") || [
     "",
   ];
-  const cities = ["sandiego", "orangecounty", "losangeles"];
+
+  const cities = (context.query.cities as string | undefined)?.split(",") || [
+    "sandiego",
+    "orangecounty",
+    "losangeles",
+  ];
+
   const queries = [];
 
   breeds.forEach((breed) => {
@@ -25,33 +31,40 @@ export const getServerSideProps: GetServerSideProps = async (context) => {
     props: {
       searchResults: JSON.parse(JSON.stringify(searchResults)),
       breeds,
+      cities,
     },
   };
 };
 
-const Puppies = ({ searchResults, breeds }) => (
-  <>
-    <Head>
-      <title>{breeds.join(", ")}</title>
-    </Head>
-    <div className="container mx-auto">
-      <div className="grid grid-cols-4 gap-10">
-        {searchResults.map((r) => (
-          <div className="w-100 border border-dashed border-gray" key={r.pid}>
-            <a href={r.url} className="">
-              <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
-              <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
-              <img
-                className="place-self-center max-h-72"
-                src={(r.images || [""])[0]}
-                alt=""
-              />
-            </a>
-          </div>
-        ))}
+const Puppies = ({ searchResults, breeds, cities }) => {
+  const title = `${breeds.join(", ")} in ${cities.join(", ")}`;
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <div className="container mx-auto">
+        <div className="grid mb-8">
+          <h1 className="place-self-center text-lg font-bold">{title}</h1>
+        </div>
+        <div className="grid grid-cols-4 gap-10">
+          {searchResults.map((r) => (
+            <div className="w-100 border border-dashed border-gray" key={r.pid}>
+              <a href={r.url} className="">
+                <h2>{`${r.title} - ${r.price} ${r.location}`}</h2>
+                <h3>{new Date(r.postedAt).toLocaleDateString("en-US")}</h3>
+                <img
+                  className="place-self-center max-h-72"
+                  src={(r.images || [""])[0]}
+                  alt=""
+                />
+              </a>
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  </>
-);
+    </>
+  );
+};
 
 export default Puppies;
